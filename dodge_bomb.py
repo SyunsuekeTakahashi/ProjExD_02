5c615e8583e25fa6373ed156581821f250501e93
takahashi <c0b2208837@edu.teu.ac.jp>
2023-04-25 17:17:44 +0900

    追加課題

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,10 +2,10 @@ import pygame as pg
 import sys
 import random
 
-delta ={pg.K_UP: (0, -1),  #練習4
-        pg.K_DOWN: (0,+1),
-        pg.K_LEFT: (-1, 0),
-        pg.K_RIGHT: (+1,0),}
+delta = {pg.K_UP: (0, -1),  #練習4
+         pg.K_DOWN: (0,+1),
+         pg.K_LEFT: (-1, 0),
+         pg.K_RIGHT: (+1,0),}
 
 def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
     """画面内外の判定
@@ -38,36 +38,42 @@ def main():
     vx, vy = +1, +1 # 練習3
     bb_rct = bb_img.get_rect()
     bb_rct.center = x, y
-
+    
     tmr = 0
+    
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return 0
-
+        
         tmr += 1
         key_lst = pg.key.get_pressed()
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
+            # if mv == (-1,0):
+                # kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
         if check_bound(screen.get_rect(), kk_rct) != (True, True):
             for k, mv in delta.items():
                 if key_lst[k]:
                     kk_rct.move_ip(-mv[0], -mv[1])
+        
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(screen.get_rect(), bb_rct)
         if not yoko:
             vx *= -1
-        if not tate -1:
+        if not tate:
             vy *= -1
         screen.blit(bb_img, bb_rct)
+        
+        if kk_rct.colliderect(bb_rct):
+            return
 
         pg.display.update()
         clock.tick(1000)
-
 
 if __name__ == "__main__":
     pg.init()
