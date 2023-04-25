5b320925640da28ddc3bc9cafbe1c7f1eafddf62
takahashi <c0b2208837@edu.teu.ac.jp>
2023-04-25 14:38:16 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,10 @@ def main():
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
     x,y = random.randint(0,1600), random.randint(0,900)
+    # screen.blit(bb_img, (x, y)) # 練習2
+    vx, vy = +1, +1
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = x, y
 
     tmr = 0
 
@@ -24,7 +28,9 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, (x, y))
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+
         pg.display.update()
         clock.tick(1000)
 
